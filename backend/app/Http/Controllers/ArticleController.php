808ee82444b842f02f78b264e41fa04523531311
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-11-25 23:19:37 +0200

    Laravel <> Add - update article method

--- backend/app/Http/Controllers/ArticleController.php
+++ backend/app/Http/Controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\News;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
@@ -52,6 +53,54 @@ class ArticleController extends Controller
                 "message" => "An error occurred while creating the article",
                 "error" => $e->getMessage(),
             ],500);
+        }
+    }
+
+    function update_article(Request $request){
+        try{
+            $article = Article::find($request->id);
+
+            if (!$article) {
+                return response()->json([
+                    "status" => "error",
+                    "message" => "Article not found"
+                ], 404);
+            }
+
+            $article->title = $request->title;
+            $article->content = $request->content;
+            
+            if ($request->hasFile('attachment')) {
+                if ($article->attachment && Storage::exists($article->attachment)) {
+                    Storage::delete($article->attachment);
+                }
+                $article->attachment = $request->file('attachment')->storeAs(
+                    "attachments/articles", 
+                    uniqid() . '.' . $request->file('attachment')->getClientOriginalExtension()
+                );
+            }
+
+            $success = $article->save();
+
+            if (!$success) {
+                return response()->json([
+                    "status" => "error",
+                    "message" => "Failed to update article"
+                ], 500);
+            }
+
+            return response()->json([
+                "status" => "success",
+                "message" => "News updated successfully",
+                "updated_article" => $article,
+            ]);
+        }catch (\Exception $e) {
+            // Handle exceptions
+            return response()->json([
+                "status" => "error",
+                "message" => "An error occurred while updating the article",
+                "error" => $e->getMessage(),
+            ], 500);
         }
     }
 }
