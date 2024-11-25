7cef68ee69a3ca669607be3dc429e801e9fdd50f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-11-25 16:24:08 +0200

    Laravel <> Create - table news

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained(table: 'users')->cascadeOnDelete();
+            $table->foreignId('news_id')->constrained('news')->cascadeOnDelete();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('role', ['admin', 'user'])->default('user');
-            $table->foreignId('user_id')->constrained(table: 'users')->cascadeOnDelete();
-            $table->foreignId('news_id')->constrained('news')->cascadeOnDelete();
             $table->date('date_of_birth')->nullable();
             $table->rememberToken();
             $table->timestamps();
